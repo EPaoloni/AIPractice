497a78442de45609163217679dca503099fcfdb7
Ezequiel Paoloni <ezequielpaoloni@outlook.com>
2020-04-19 00:55:40 -0300

    Added my own NeuralNetwork

---
+++ Flappy_Bird/MyNeuralNetwork/Matrix.py
@@ -0,0 +1,118 @@
+import random
+
+class Matrix:
+
+  def __init__(self, rows, cols):
+    self.rows = rows
+    self.cols = cols
+    self.data = []
+    
+    vector = []
+    for i in range(0, rows):
+      for j in range(0, cols):
+        vector.append(0)
+      self.data.append(vector)
+      vector = []
+
+
+  def add(self, n):
+    if isinstance(n, Matrix):
+      for i in range(0, self.rows):
+        for j in range(0, self.cols):
+          self.data[i][j] += n.data[i][j]
+    else:  
+      for i in range(0, self.rows):
+        for j in range(0, self.cols):
+          self.data[i][j] += n
+    
+
+  def scalarMultiply(self, n):
+    for i in range(0, self.rows):
+      for j in range(0, self.cols):
+        self.data[i][j] *= n
+
+  
+  def map(self, fn):
+    # Apply a function to every element of matrix
+    for i in range(0, self.rows):
+      for j in range(0, self.cols):
+        val = self.data[i][j]
+        self.data[i][j] = fn(val)
+
+
+  def randomize(self):
+    for i in range(0, self.rows):
+      for j in range(0, self.cols):
+        self.data[i][j] = random.uniform(0, 1)
+
+
+  @staticmethod
+  def mapMatrix(matrix, fn):
+    result = Matrix(matrix.rows, matrix.cols)
+    for i in range(0, result.rows):
+      for j in range(0, result.cols):
+        val = matrix.data[i][j]
+        result.data[i][j] = fn(val)
+    return result
+  
+  @staticmethod
+  def transpose(matrix):
+    result = Matrix(matrix.cols, matrix.rows)
+    for i in range(0, matrix.rows):
+      for j in range(0, matrix.cols):
+        result.data[j][i] = matrix.data[i][j]
+    return result
+
+
+  @staticmethod
+  def subtract(a, b):
+    # Return a new Matrix A-B
+    result = Matrix(a.rows, a.cols)
+    for i in range(0, result.rows):
+      for j in range(0, result.cols):
+        result.data[i][j] = a.data[i][j] - b.data[i][j]
+
+    return result
+    
+
+
+  @staticmethod
+  def fromArray(arr):
+    m = Matrix(arr.__len__(), 1)
+    for i in range(0, arr.__len__()):
+      m.data[i][0] = arr[i]
+    
+    return m
+
+
+  @staticmethod
+  def toArray(matrix):
+    if matrix.cols is 1:
+      arr = []
+      for i in range(0, matrix.data.__len__()):
+        arr.append(matrix.data[i][0])
+      return arr
+    else:
+      print("Too many cols, cannot convert")
+      return None
+
+
+  @staticmethod
+  def matrixMultiply(a, b):
+    result = Matrix(a.rows, b.cols)
+    for i in range(0, result.rows):
+      for j in range(0, result.cols):
+        sum = 0
+        for k in range(0, a.cols):
+          sum += a.data[i][k] * b.data[k][j]
+        result.data[i][j] = sum
+    return result
+
+
+  def __str__(self):
+    string = ""
+    for array in self.data:
+      string += (array.__str__() + "\n")
+
+    return string
+    
---
+++ Flappy_Bird/MyNeuralNetwork/NeuralNetwork.py
@@ -0,0 +1,101 @@
+from MyNeuralNetwork.Matrix import Matrix
+from math import exp
+
+def sigmoid(x):
+  return 1 / (1 + exp(-x))
+
+def derivativeSigmoid(y):
+  #return sigmoid(x) * (1 - sigmoid(x))
+  return y * (1 - y)
+
+class NeuralNetwork:
+
+  def __init__(self, input_nodes, hidden_nodes, output_nodes):
+    self.input_nodes = input_nodes
+    self.hidden_nodes = hidden_nodes
+    self.output_nodes = output_nodes
+
+    self.weights_ih = Matrix(self.hidden_nodes, self.input_nodes)
+    self.weights_ih.randomize()
+    self.weights_ho = Matrix(self.output_nodes, self.hidden_nodes)
+    self.weights_ho.randomize()
+
+    self.bias_h = Matrix(self.hidden_nodes, 1)
+    self.bias_h.randomize()
+    self.bias_o = Matrix(self.output_nodes, 1)
+    self.bias_o.randomize()
+
+    self.learning_rate = 1
+
+    
+  
+  def feedForward(self, input_array):
+    # Generating the hidden outputs
+    inputs = Matrix.fromArray(input_array)
+    hidden = Matrix.matrixMultiply(self.weights_ih, inputs)
+    hidden.add(self.bias_h)
+    # Activation function
+    hidden.map(sigmoid)
+
+    # Generating the output of the NN
+    output = Matrix.matrixMultiply(self.weights_ho, hidden)
+    output.add(self.bias_o)
+    output.map(sigmoid)
+
+    return Matrix.toArray(output)
+
+  
+  def train(self, input_array, target_array):
+    # Generating the hidden outputs
+    inputs = Matrix.fromArray(input_array)
+    hidden = Matrix.matrixMultiply(self.weights_ih, inputs)
+    hidden.add(self.bias_h)
+    
+    # Activation function
+    hidden.map(sigmoid)
+
+    # Generating the output of the NN
+    outputs = Matrix.matrixMultiply(self.weights_ho, hidden)
+    outputs.add(self.bias_o)
+    outputs.map(sigmoid)
+    
+    # Convert array to matrix object
+    targets = Matrix.fromArray(target_array)
+
+    # Calculate the error
+    # ERROR = TARGETS - OUTPUTS
+    output_errors = Matrix.subtract(targets, outputs)
+    
+    # Calculate Gradient Descent
+    gradients = Matrix.mapMatrix(outputs, derivativeSigmoid)
+
+    # TODO: Aca gradients y output_errors son los dos de 2x1, ver esto bien
+    gradients = Matrix.matrixMultiply(gradients, output_errors)
+    gradients.scalarMultiply(self.learning_rate)
+    self.bias_o.add(gradients)
+
+    # Calculate hidden->output deltas
+    hidden_t = Matrix.transpose(hidden)
+    weights_ho_deltas = Matrix.matrixMultiply(gradients, hidden_t)
+
+    # Adjust hidden->output weights
+    self.weights_ho.add(weights_ho_deltas)
+
+    # Calculate the hidden layer errors
+    weights_ho_t = Matrix.transpose(self.weights_ho)
+    hidden_errors = Matrix.matrixMultiply(weights_ho_t, output_errors)
+
+    # Calculate hidden gradient
+    hidden_gradient = Matrix.mapMatrix(hidden, derivativeSigmoid)
+    hidden_gradient = Matrix.matrixMultiply(hidden_gradient, hidden_errors)
+    hidden_gradient.scalarMultiply(self.learning_rate)
+    self.bias_h.add(hidden_gradient)
+
+    # Calculate input->hidden deltas
+    inputs_t = Matrix.transpose(inputs)
+    weights_ih_deltas = Matrix.matrixMultiply(hidden_gradient, inputs_t)
+
+    # Adjust input->hidden weights
+    self.weights_ih.add(weights_ih_deltas)
+
+    
